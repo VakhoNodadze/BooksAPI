f92d5ba5c961dc95600f1498e91905ff2228a99b
VakhoNodadze <vakhtang.nodadze@geolab.edu.ge>
2019-05-24 17:02:31 +0400

    Updates

---
+++ precache-manifest.be1739284a0cdbec2c9958094d407ce2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9629547f9ba9f62f0233dfcfbb2d3e79",
+    "url": "/BooksAPI/index.html"
+  },
+  {
+    "revision": "a8cde23a79a821df0ad7",
+    "url": "/BooksAPI/static/css/main.77aa42f8.chunk.css"
+  },
+  {
+    "revision": "a1080fe6a76bf6e98d9d",
+    "url": "/BooksAPI/static/js/2.d56e4ec4.chunk.js"
+  },
+  {
+    "revision": "a8cde23a79a821df0ad7",
+    "url": "/BooksAPI/static/js/main.1153ee80.chunk.js"
+  },
+  {
+    "revision": "86667ed47b93a5e4c316",
+    "url": "/BooksAPI/static/js/runtime~main.04973a97.js"
+  }
+]);
