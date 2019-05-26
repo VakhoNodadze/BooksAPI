f795696fc7ed16c3f6d2e2c8f8f4ee7d1af0f428
VakhoNodadze <vakhtang.nodadze@geolab.edu.ge>
2019-05-26 23:05:03 +0400

    added search bar

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,33 +1,64 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
-import { toggleFavourite } from "../actions/action";
+import {
+  toggleFavourite,
+  fetchBooksSuccess,
+  fetchBooks
+} from "../actions/action";
 import styles from "../styles/App.module.scss";
 import Book from "./EachBook";
 import Loader from "./Loader";
 
-const Main = props => {
-  const { books } = props.bookState;
-
-  const handleFavourite = id => {
-    props.toggleFavourite(id);
+class Main extends Component {
+  state = {
+    search: ""
   };
-  if (books.length === 0) return <Loader />;
-  return (
-    <React.Fragment>
-      <main className={styles.main}>
-        {books.map(book => {
-          return (
-            <Book
-              key={book.id}
-              book={book}
-              onLike={bookId => handleFavourite(bookId)}
-            />
-          );
-        })}
-      </main>
-    </React.Fragment>
-  );
-};
+  componentDidMount() {
+    console.log(this.props);
+  }
+  updateSearch = e => {
+    this.setState({ search: e.target.value });
+  };
+  render() {
+    const { books } = this.props.bookState;
+    const filteredBooks = books.filter(book => {
+      return (
+        book.volumeInfo.title.toLowerCase().indexOf(this.state.search) != -1
+      );
+    });
+
+    const handleFavourite = id => {
+      this.props.toggleFavourite(id);
+    };
+    if (books.length === 0) return <Loader />;
+
+    return (
+      <React.Fragment>
+        <div className={styles.container}>
+          <input
+            type="text"
+            placeholder="Search..."
+            value={this.state.search}
+            onChange={this.updateSearch}
+          />
+          <div className={styles.search} />
+        </div>
+        <main className={styles.main}>
+          {filteredBooks.map(book => {
+            return (
+              <Book
+                key={book.id}
+                book={book}
+                onLike={bookId => handleFavourite(bookId)}
+              />
+            );
+          })}
+          {console.log(filteredBooks)}
+        </main>
+      </React.Fragment>
+    );
+  }
+}
 
 const mapStateToProps = state => {
   return { bookState: state };
@@ -35,7 +66,5 @@ const mapStateToProps = state => {
 
 export default connect(
   mapStateToProps,
-  { toggleFavourite }
+  { toggleFavourite, fetchBooks }
 )(Main);
-
-// export default Main;
